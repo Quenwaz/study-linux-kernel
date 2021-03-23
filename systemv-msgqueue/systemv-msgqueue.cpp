6af9677b4723b12913fcebece1707e3f47ca3c9a
坤华 章 <zhangkunhua3696@126.com>
2021-03-23 23:10:55 +0800

    message queue

--- systemv-msgqueue/systemv-msgqueue.cpp
+++ systemv-msgqueue/systemv-msgqueue.cpp
@@ -1,8 +1,62 @@
 #include <cstdlib>
+#include <cstdio>
+#include <unistd.h>
+#include <string.h> // for memset
+#include <sys/msg.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
+struct __msgbuf
+  {
+	  enum{
+		  MSG_LEN = 1024
+	  };
+
+    __syscall_slong_t mtype;	/* type of received/sent message */
+    char mtext[MSG_LEN];		/* text of the message */
+  };
 
 int main(int argc, char* argv[])
 {
-	
+	key_t key = ftok(argv[0], 1);
+	int msgId = msgget(key, IPC_CREAT | S_IRUSR | S_IWUSR);
+	pid_t pidChild = -1;
+	switch (pidChild = fork())
+	{
+	case -1:
+		return EXIT_FAILURE;
+	case 0:	
+	{
+		msgId = msgget(key, IPC_EXCL | S_IRUSR | S_IWUSR);
+		if (msgId == -1){
+			printf("get msg failed\n");
+			return EXIT_FAILURE;
+		}
+		printf("pid: %d, msg id :%d\n", getpid(), msgId);
+		__msgbuf buf;
+		memset(&buf, 0, sizeof(__msgbuf));
+		auto msg = "test msg queue";
+		if (argc > 1) msg = argv[1];
+		strncpy(buf.mtext, msg, strlen(msg));
+		buf.mtype = 1024;
+		msgsnd(msgId, &buf, sizeof(__msgbuf), 0);
+		printf("pid: %d, send msg done\n", getpid());
+	}
+		
+		exit(0);
+	default:
+		break;
+	}	
+
+	printf("msg key: %ld\n", key);
+	printf("pid: %d, msg id :%d\n", getpid(), msgId);
+	__msgbuf msg;
+	msg.mtype = 1024;
+	// The call will block until the specified type of message is received.
+	msgrcv(msgId,&msg, sizeof(msg), msg.mtype, 0);
+	printf("pid: %d, recv msg:%s\n", getpid(), msg.mtext);
+
+	// Delete message queue
+	msgctl(msgId, IPC_RMID, NULL);
 	return EXIT_SUCCESS;
 }
