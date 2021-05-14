d62ad5a3a4bb8ee53a626636ee18ffef70a92079
Quenwaz <zhangkunhua3696@126.com>
2021-05-14 14:32:13 +0800

    adjust server to select i/o

--- socket/socket-server-tcp-select.cpp
+++ socket/socket-server-tcp-select.cpp
@@ -10,51 +10,107 @@
 #include <stdlib.h>
 #include <string>
 #include <errno.h>
+#include <fcntl.h>
 #include <sys/select.h>
+#include <set>
+#include <deque>
+#include <utility>
+
+
 // inet_ntoa <=> inet_addr
 // ntohs <=> htons
 
+pthread_mutex_t g_mtx = PTHREAD_MUTEX_INITIALIZER;
 
 void* _thread_func(void * pData)
 {
-	int sock = *(int*)(pData);
+	std::set<int> * pSetFdAccepted = (std::set<int>*)(pData);
+	std::deque<std::pair<int, std::string>> queMsgs;
 	for(;;)
 	{
-		char szMsg[1024] = { 0 };
-		ssize_t _n_read = recv(sock, szMsg, sizeof(szMsg), 0);
-		if (_n_read == -1)
-		{
-			fprintf(stderr, "Error: recv failed: %s\n", strerror(errno));
-			break;
-		}
-		fprintf(stderr, "recv %d bytes msg: %s\n", _n_read, szMsg);
-		fprintf(stderr, "\n>:");
 
-		memset(szMsg, 0, _n_read);
-		_n_read = read(STDIN_FILENO, szMsg, sizeof(szMsg));
-		if (_n_read <= 0)
-		{
+		fd_set setrd, setwd;
+		FD_ZERO(&setrd);
+		FD_ZERO(&setwd);
+
+		int maxfd = STDIN_FILENO;
+		FD_SET(STDIN_FILENO, &setrd);
+		for (auto iter = pSetFdAccepted->begin(); iter != pSetFdAccepted->end(); ++iter){
+			FD_SET(*iter, &setrd);
+			FD_SET(*iter, &setwd);
+			if (*iter > maxfd){
+				maxfd = *iter + 1;
+			}
+		}
+
+		int ret = select(maxfd, &setrd, &setwd, NULL, NULL);
+		if (ret <= 0){
 			continue;
 		}
 
-		if (_n_read == 2 && szMsg[0] == 'q')
-		{
-			break;
+
+		if (FD_ISSET(STDIN_FILENO, &setrd)){
+			queMsgs.push_back(std::make_pair(pSetFdAccepted->size(), ""));
+			queMsgs.back().second.resize(1024, 0);
+			read(STDIN_FILENO, &queMsgs.back().second[0], 1024);
 		}
 
-		// 使用MSG_NOSIGNAL flag 避免对端socket关闭后再发送导致进程kill
-		_n_read = send(sock, szMsg, strlen(szMsg), 0);
-		if (_n_read == -1)
-		{
-			fprintf(stderr, "Error: send failed: %s\n", strerror(errno));
-			break;
-		}
 
-		fprintf(stderr, "[%lu] send %d bytes msg: %s\n", pthread_self(), _n_read, szMsg);
+		pthread_mutex_lock(&g_mtx);
+		for (auto iter = pSetFdAccepted->begin(); iter != pSetFdAccepted->end(); ){
+			bool bClosed = false;
+			if (FD_ISSET(*iter, &setwd))
+			{
+				for (int i =queMsgs.size() - 1;i >= 0; --i)
+				{
+					if (queMsgs.at(i).first <= 0){
+						continue;
+					}
+					
+					auto msg = queMsgs.at(i).second.c_str();
+					ssize_t _n_read = send(*iter, msg, strlen(msg), 0);
+					if (_n_read == -1)
+					{
+						fprintf(stderr, "Error: send failed: %s\n", strerror(errno));
+						bClosed = true;
+						break;
+					}
+					--queMsgs.at(i).first;
+					fprintf(stderr, "send %d bytes msg: %s\n", _n_read, msg);
+				}
+			}
+
+
+			if (FD_ISSET(*iter, &setrd)){
+				char msg[1024]= {0};
+				ssize_t _n_read = recv(*iter, msg, sizeof(msg), 0);
+				if (_n_read <= 0)
+				{
+					fprintf(stderr, "Error: recv failed: %s\n", strerror(errno));
+					bClosed = true;		
+				}else{
+					fprintf(stderr, "recv msg %d bytes: %s\n", _n_read, msg);
+				}
+			}
+
+						
+			if (bClosed){
+				close(*iter);
+				iter = pSetFdAccepted->erase(iter);
+			}else ++iter;
+		}
+		pthread_mutex_unlock(&g_mtx);
+
+		for (auto iter = queMsgs.begin(); iter != queMsgs.end();)
+		{
+			if (iter->first <= 0){
+				iter = queMsgs.erase(iter);
+			}else ++iter;
+		}
+		
 	}
 
 	fprintf(stderr, "\n[%lu] goodbye\n", pthread_self());
-	close(sock);
 	return NULL;
 }
 
@@ -105,47 +161,30 @@ int main(int argc, char* argv[])
 	// 忽略SIGPIPE信号， 避免对端socket关闭后， 触发此信号导致进程终止
 	signal(SIGPIPE, SIG_IGN);
 	
+	
+	fcntl(STDIN_FILENO,F_SETFL,  fcntl(STDIN_FILENO, F_GETFL) | O_NONBLOCK);
 	fprintf(stderr, "listen to: 0.0.0.0:%s\n", argv[1]);
+
+	std::set<int> setFdAccepted;
 	for (;;)
 	{
 		struct sockaddr_in addr_client;
 		socklen_t _n_addr_client = 0;
-		int sock_client = accept(sock, (struct sockaddr*)&addr_client, &_n_addr_client);
+		const int sock_client = accept(sock, (struct sockaddr*)&addr_client, &_n_addr_client);
 		if (-1 == sock_client)
 		{
 			continue;
 		}
 		
+		fcntl(sock_client,F_SETFL,  fcntl(sock_client, F_GETFL) | O_NONBLOCK);
+		pthread_mutex_lock(&g_mtx);
+		setFdAccepted.insert(sock_client);
+		pthread_mutex_unlock(&g_mtx);
+
 		fprintf(stderr, "accept %s connect\n", inet_ntoa(addr_client.sin_addr));
-		
-		fd_set setrd, setwd;
-		FD_ZERO(&setrd);
-		FD_ZERO(&setwd);
-
-		int arrFds[2] = {STDIN_FILENO, sock_client};
-		const int nfds = sizeof(arrFds)/sizeof(arrFds[0]);
-		for (size_t i =0; i < nfds; ++i){
-			FD_SET(arrFds[i], &setrd);
-			FD_SET(arrFds[i], &setwd);
-		}
-
-		int ret = select(nfds, &setrd, &setwd, NULL, NULL);
-		if (ret <= 0){
-			continue;
-		}
-
-		for(size_t i =0;i < nfds; ++i){
-			if (FD_ISSET(arrFds[i], &setrd)){
-
-			}
-
-			if (FD_ISSET(arrFds[i], &setwd)){
-
-			}
-		}
 
 		pthread_t thread;
-		if( 0 > pthread_create(&thread, NULL, _thread_func, &sock_client))
+		if( 0 > pthread_create(&thread, NULL, _thread_func, &setFdAccepted))
 		{
 			
 			continue;
@@ -158,3 +197,4 @@ int main(int argc, char* argv[])
 
 	return EXIT_SUCCESS;
 }
+
