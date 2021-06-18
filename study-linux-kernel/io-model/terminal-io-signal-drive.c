c512e71281418709352eb6432cc9a89a6fa8d0d5
Quenwaz <zhangkunhua3696@126.com>
2021-06-18 10:42:55 +0800

    Learning signal to dirve I/O.

--- study-linux-kernel/io-model/terminal-io-signal-drive.c
+++ study-linux-kernel/io-model/terminal-io-signal-drive.c
@@ -1,47 +1,64 @@
+/**
+ * @file terminal-io-signal-drive.c
+ * @author Quenwaz (404937333@qq.com)
+ * @brief 终端输入之信号驱动I/O
+ * @version 0.1
+ * @date 2021-06-18
+ * 需使用实时信号， 否则不会排队造成丢失
+ * @copyright Copyright (c) 2021 Quenwaz
+ * 
+ */
 #define _GNU_SOURCE
 #include <signal.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdio.h>
 
-static volatile sig_atomic_t gotsigio = 0;
+#define SIG_DRIVE_IO (SIGRTMIN+2)
 
-static void sigHandler(int sig)
+static void sigHandler(int sig, siginfo_t * siginfo, void * data)
 {
-    gotsigio = 1;
+    char msg[1024]={0};
+    ssize_t numread = read(siginfo->si_fd, msg, sizeof msg);
+    if (numread > 0)
+        write(STDERR_FILENO, msg,numread);
 }
 
 int main(int argc, char const *argv[])
-{
+{   
+    fprintf(stderr, "非阻塞终端输入之信号驱动测试程序\n\n");
+    /// step 1: 设置信号处理器
     struct sigaction sa;
     sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_RESTART;
-    sa.sa_handler = sigHandler;
-    if(sigaction(SIGIO, &sa, NULL) == -1){
+    sa.sa_flags = SA_RESTART |SA_SIGINFO;
+    sa.sa_sigaction = sigHandler;
+    if(sigaction(SIG_DRIVE_IO, &sa, NULL) == -1){
         return 1;
     }
 
-    if(fcntl(STDIN_FILENO, F_SETOWN, getpid()) == -1){
+    /// step 2: 指定文件描述符的信号及信号接收进程
+    if( fcntl(STDIN_FILENO, F_SETSIG, SIG_DRIVE_IO) ==-1 ||
+        fcntl(STDIN_FILENO, F_SETOWN, getpid()) == -1){
         return 1;
     }
 
+    /// step 3: 设置文件描述符异步非阻塞
     int flags = fcntl(STDIN_FILENO, F_GETFL);
     if (fcntl(STDIN_FILENO, F_SETFL, flags | O_ASYNC | O_NONBLOCK) == -1){
         return 1;
     }
 
-    while (gotsigio == 0)
+    // step 4 循环阻塞非SIG_DRIVE_IO信号， 以等待SIG_DRIVE_IO信号
+    sigset_t set_block, setold;
+    sigemptyset(&set_block);
+    sigaddset(&set_block, SIG_DRIVE_IO);
+    sigprocmask(SIG_BLOCK, &set_block, &setold);
+    for(;;)
     {
-        pause();
-        if (gotsigio){
-            char msg[1024]={0};
-            ssize_t numread = read(STDIN_FILENO, msg, sizeof msg);
-            if (numread > 0)
-                write(STDERR_FILENO, msg,numread);
-            gotsigio = 0;  
-        }
-
-       
+        sigsuspend(&setold);
+        // sigwaitinfo();
+        // pause();
     }
-    
+    sigprocmask(SIG_UNBLOCK, &set_block, NULL);
     return 0;
 }
