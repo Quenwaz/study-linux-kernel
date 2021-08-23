f110bc511479b7aa93c572e2338e666508f3e216
Quenwaz <zhangkunhua3696@126.com>
2021-08-23 19:47:30 +0800

    Initialize UNIX domain socketpair.

---
+++ study-linux-kernel/socket/socket_unixdomain_pair.c
@@ -0,0 +1,23 @@
+/**
+ * @file socket_unixdomain_pair.c
+ * @author Quenwaz (404937333@qq.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-08-23
+ * 
+ * @copyright Copyright (c) 2021 Quenwaz
+ * 
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/socket.h>
+
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
